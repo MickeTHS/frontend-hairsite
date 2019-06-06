b6e64347920cc363460939d45d2c0a1d06c335be
boushib <boushib01@gmail.com>
2019-06-06 02:16:37 +0000

    Added Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,85 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Navbar from "@/components/shared/Navbar";
+export default {
+  components: {
+    Navbar
   }
+};
+</script>
+
+<style lang="scss">
+$primaryColor: #5ec0ea;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+}
+
+p,
+li,
+label,
+a {
+  font-size: 15px;
+  color: #777;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+input {
+  outline: none;
+}
+
+input::placeholder {
+  color: #fff;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="tel"],
+input[type="password"] {
+  border: none;
+  background-color: rgba(0, 0, 0, 0.08);
+  color: #fff;
+  font-size: 14px;
+  padding: 14px 28px;
+  border-radius: 25px;
+}
+
+input:-internal-autofill-selected {
+  background-color: rgba(0, 0, 0, 0.08) !important;
+  color: #fff !important;
+}
+
+.btn {
+  border: none;
+  border-radius: 30px;
+  background-color: $primaryColor;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 15px;
+  font-weight: 500;
+  cursor: pointer;
+  outline: none;
+}
+
+.container {
+  padding: 0 80px;
 }
 </style>
---
+++ src/components/shared/Navbar.vue
@@ -0,0 +1,125 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="brand">
+        <a href="./">HairPlace</a>
+      </div>
+      <ul class="top-menu">
+        <li>
+          <a href>Home</a>
+        </li>
+        <li>
+          <a href>About</a>
+        </li>
+        <li>
+          <a href>Services</a>
+        </li>
+        <li>
+          <a href>Contact</a>
+        </li>
+      </ul>
+      <form action>
+        <button class="btn">Sign In</button>
+      </form>
+      <div class="collapse">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </nav>
+</template>
+<style lang="scss">
+$primaryColor: #5ec0ea;
+nav {
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 54px;
+  }
+
+  .brand {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 500;
+    color: $primaryColor;
+    cursor: pointer;
+    a {
+      font-size: inherit;
+      color: inherit;
+    }
+  }
+
+  .top-menu {
+    li {
+      display: inline-block;
+      margin: 18px 10px;
+
+      a {
+        color: #999;
+        font-weight: 400;
+      }
+    }
+  }
+
+  form {
+    .btn {
+      background-color: #fff;
+      border: 1px solid #42afde;
+      color: #42afde;
+    }
+  }
+
+  .collapse {
+    display: none;
+    position: relative;
+    width: 20px;
+    height: 14px;
+    cursor: pointer;
+
+    span {
+      height: 2px;
+      width: 20px;
+      position: absolute;
+      left: 0;
+      background-color: $primaryColor;
+
+      &:first-child {
+        top: 0;
+      }
+
+      &:nth-child(2),
+      &:nth-child(3) {
+        top: 6px;
+      }
+
+      &:last-child {
+        top: 12px;
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  nav {
+    .top-menu,
+    form {
+      display: none;
+    }
+
+    .collapse {
+      display: block;
+    }
+  }
+}
+</style>
